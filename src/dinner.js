7663bf68035f54c02d2eac1776fec6a8a1d5c772
KJ <kamranajabbar@gmail.com>
2020-12-15 00:04:57 +0500

    updated dinner.js file with some text

--- src/dinner.js
+++ src/dinner.js
@@ -1,7 +1,7 @@
 function Dinner(probs) {
     return (
           <div>
-              <p>Dear visitor, we are happy to announced that your day <b>{probs.Day}</b> menu for dinner will be <b>{probs.dishName}</b> and in deserts <b>{probs.desertsName}</b> will be served.</p>
+              <p>Dear valueable visitor, we are happy to announced that your day <b>{probs.Day}</b> menu for dinner will be <b>{probs.dishName}</b> and in deserts <b>{probs.desertsName}</b> will be served.</p>
           </div>
       );
   }
