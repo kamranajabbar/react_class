7c1cd15768a5e68f7f2899d16e9d082e11b1297c
KJ <kamranajabbar@gmail.com>
2020-12-16 19:06:34 +0500

    Class 4 has been ended with some new code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Message} from './Message';
 import './App.css';
-import Dinner from './dinner';
 
 // Stateless Components
 function App() {
+  let [count, setCount] = useState(0);
+  let [isMorning, setMorning] = useState(true);
+
   return (
-    <div className="App">
-      <Dinner Day="One" dishName="Chicken Karahi" desertsName="Kheer" />
-      <Dinner Day="Two" dishName="Chicken Biryani" desertsName="Gajar Kaa Halwa" />
-      <Dinner Day="Three" dishName="Beef Biryani" desertsName="Rus Golla" />
+    /* <div className={`box dayLight`}>
+       <div className="box dayLight"> 
+       <div className={`box ${isMorning ? 'dayLight' : ''}`}> */
+    
+    <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+      <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
+
+      <Message counter={count} />
+
+      <br />
+
+      <button onClick = { 
+          // () => setCount(count + 1) //Note: Same as below
+          () => setCount(++count) 
+        }>
+        Update Counter
+      </button>
+
+      <button onClick = { 
+          () => setMorning(!isMorning) 
+        }>
+        Update Day
+      </button>
     </div>
   );
 }
